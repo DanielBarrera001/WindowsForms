f0302cb09b8034cc89a252db6c74867e60d3cc5c
Daniel <barreraquezadadanielalejandro@gmail.com>
2025-06-23 17:44:53 -0600

    Noveno commit, TryCatch

--- Proyecto con Windows Forms/Form1.Designer.cs
+++ Proyecto con Windows Forms/Form1.Designer.cs
@@ -32,21 +32,26 @@
             Btn_Close = new Button();
             lbl_texto = new Label();
             txt_Nombre = new TextBox();
+            label1 = new Label();
+            txt_Edad = new TextBox();
+            btn_Confirm = new Button();
+            lblConfirm = new Label();
+            btn_Editar = new Button();
             SuspendLayout();
             // 
             // Btn_OK
             // 
-            Btn_OK.Location = new Point(140, 396);
+            Btn_OK.Location = new Point(140, 374);
             Btn_OK.Name = "Btn_OK";
             Btn_OK.Size = new Size(75, 23);
             Btn_OK.TabIndex = 0;
-            Btn_OK.Text = "Ok";
+            Btn_OK.Text = "Registrar datos";
             Btn_OK.UseVisualStyleBackColor = true;
             Btn_OK.Click += Btn_OK_Click;
             // 
             // Btn_Close
             // 
-            Btn_Close.Location = new Point(221, 396);
+            Btn_Close.Location = new Point(221, 374);
             Btn_Close.Name = "Btn_Close";
             Btn_Close.Size = new Size(75, 23);
             Btn_Close.TabIndex = 1;
@@ -57,11 +62,11 @@
             // lbl_texto
             // 
             lbl_texto.AutoSize = true;
-            lbl_texto.Location = new Point(25, 30);
+            lbl_texto.Location = new Point(25, 252);
             lbl_texto.Name = "lbl_texto";
-            lbl_texto.Size = new Size(66, 15);
+            lbl_texto.Size = new Size(12, 15);
             lbl_texto.TabIndex = 2;
-            lbl_texto.Text = "Bienvenido";
+            lbl_texto.Text = "-";
             // 
             // txt_Nombre
             // 
@@ -70,11 +75,60 @@
             txt_Nombre.Size = new Size(100, 23);
             txt_Nombre.TabIndex = 3;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 105);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Edad:";
+            // 
+            // txt_Edad
+            // 
+            txt_Edad.Location = new Point(25, 138);
+            txt_Edad.Name = "txt_Edad";
+            txt_Edad.Size = new Size(100, 23);
+            txt_Edad.TabIndex = 5;
+            // 
+            // btn_Confirm
+            // 
+            btn_Confirm.Location = new Point(59, 374);
+            btn_Confirm.Name = "btn_Confirm";
+            btn_Confirm.Size = new Size(75, 23);
+            btn_Confirm.TabIndex = 6;
+            btn_Confirm.Text = "Confirmar";
+            btn_Confirm.UseVisualStyleBackColor = true;
+            btn_Confirm.Click += btn_Confirm_Click;
+            // 
+            // lblConfirm
+            // 
+            lblConfirm.AutoSize = true;
+            lblConfirm.Location = new Point(25, 279);
+            lblConfirm.Name = "lblConfirm";
+            lblConfirm.Size = new Size(0, 15);
+            lblConfirm.TabIndex = 7;
+            // 
+            // btn_Editar
+            // 
+            btn_Editar.Location = new Point(221, 330);
+            btn_Editar.Name = "btn_Editar";
+            btn_Editar.Size = new Size(75, 38);
+            btn_Editar.TabIndex = 8;
+            btn_Editar.Text = "Editar datos";
+            btn_Editar.UseVisualStyleBackColor = true;
+            btn_Editar.Click += button1_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(308, 431);
+            Controls.Add(btn_Editar);
+            Controls.Add(lblConfirm);
+            Controls.Add(btn_Confirm);
+            Controls.Add(txt_Edad);
+            Controls.Add(label1);
             Controls.Add(txt_Nombre);
             Controls.Add(lbl_texto);
             Controls.Add(Btn_Close);
@@ -91,5 +145,10 @@
         private Button Btn_Close;
         private Label lbl_texto;
         private TextBox txt_Nombre;
+        private Label label1;
+        private TextBox txt_Edad;
+        private Button btn_Confirm;
+        private Label lblConfirm;
+        private Button btn_Editar;
     }
 }
--- Proyecto con Windows Forms/Form1.cs
+++ Proyecto con Windows Forms/Form1.cs
@@ -2,9 +2,13 @@ namespace Proyecto_con_Windows_Forms
 {
     public partial class Form1 : Form
     {
+
+        private bool confirmarDatos = false;
+
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Btn_Close_Click(object sender, EventArgs e)
@@ -14,8 +18,44 @@ namespace Proyecto_con_Windows_Forms
 
         private void Btn_OK_Click(object sender, EventArgs e)
         {
-            string textoDelTextBox = txt_Nombre.Text;
-            lbl_texto.Text = textoDelTextBox;
+
+            try
+            {
+                string txtBoxName = txt_Nombre.Text;
+                byte txtBoxEdad = Convert.ToByte(txt_Edad.Text);
+
+                lbl_texto.Text = "Tu nombre es " + txtBoxName + " y tu edad es " + txtBoxEdad;
+                lblConfirm.Text = "Confirmas esta informacion?";
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Ingresa una edad valida"); ;
+            }
+
+            confirmarDatos |= true;
+        }
+
+        private void btn_Confirm_Click(object sender, EventArgs e)
+        {
+
+            if (confirmarDatos == true)
+            {
+                MessageBox.Show("Estas registrado");
+                Application.Exit(); 
+            }
+            else
+            {
+                MessageBox.Show("Ingresa los datos");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           txt_Edad.Clear();
+            txt_Nombre.Clear();
+            lbl_texto.Text = " ";
+            lblConfirm.Text = " ";
         }
     }
 }
